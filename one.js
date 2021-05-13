201609b8b44c3482e26936e6f2a2e98447f3fb44
Alexander561 <82186928+Alexander561@users.noreply.github.com>
2021-05-13 19:05:06 -0400

    Add files via upload

---
+++ one.js
@@ -0,0 +1,40 @@
+let equation = (100 + 4 * 11)
+console.log(equation)
+
+let question = ((100 + 4) * 11)
+console.log(question)
+
+console.log(typeof 4.5)
+console.log(typeof "x")
+
+console.log(- (10 - 2))
+
+console.log(3 > 2)
+console.log(3 < 2)
+
+console.log("Aardvark" < "Zoroaster")
+console.log("Itchy" != "Scratchy")
+console.log("Apple" == "Orange")
+
+console.log(NaN == NaN)
+
+console.log(true && false)
+console.log(true & true)
+
+console.log(false || true)
+console.log(false || false)
+
+console.log(true ? 1 : 2);
+console.log(false ? 1 : 2);
+
+console.log(8 * null)
+console.log("5" - 1)
+console.log("5" + 1)
+console.log("five" * 2)
+console.log(false == 0)
+
+console.log(null == undefined);
+console.log(null == 0);
+
+console.log(null || "user")
+console.log("Agnes" || "user")
